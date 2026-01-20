a28a74d89a785b8a1e88f57bc57fa449fff3d767
Kaustubh Pandey <166645575+kaustub-1410@users.noreply.github.com>
2026-01-20 23:14:35 +0530

    update

---
+++ HomeWork/evennumber.py
@@ -0,0 +1,5 @@
+numbers = [10, 15, 22, 33, 40, 55, 60]
+
+for i in numbers:
+    if i % 2 == 0:
+        print(i)
---
+++ HomeWork/multipletimes.py
@@ -0,0 +1,4 @@
+n = int(input("Enter the number of times to repeat the string: "))
+
+for i in range(n):
+    print("GOOD MORNING")
